21a41b5fc1700f2d011beccdade4f96171da6c1a
Alenka2704 <Alenka_2704@bigmir.net>
2016-09-08 18:12:59 +0900

    Calculate button handler. Crashes

--- app/src/main/java/com/example/culinarycalculator/MainActivity.java
+++ app/src/main/java/com/example/culinarycalculator/MainActivity.java
@@ -7,9 +7,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -18,44 +20,105 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     List<String> labels;
+    List<Integer> ids;
     ArrayList<HashMap<String, String>> productsList;
+    LinearLayout parentLayout;
+    int productsCount;
 
     //TODO prevent selection of the same item twice, order items by name. If item is selected, it's removed from other spinners lists, added back on item entry removal. Set ids for spinners items that respond ids of these products in database
 
-public String getProductId(LinearLayout parentLayout, int i){
-    LinearLayout temp = (LinearLayout) parentLayout.getChildAt(i);
-    Spinner tempSpinner = (Spinner) temp.findViewWithTag("spnProduct");
-    int k = tempSpinner.getSelectedItemPosition();
-    return productsList.get(k + 1).get("id");
+    /**
+     * as far as storing of ids for Spinner items is performed using two lists, this method is supposed to match them
+     * @param i index of current Spinner parent layout i.e. number of product on the screen
+     * @return id of product in database
+     */
+    public String getProductId(int i) {
+        int k = ((Spinner) getViewByTag("spnProduct", i)).getSelectedItemPosition();
+        return k==0?"":productsList.get(k-1).get("id");
 
-}
+    }
+
+    /**
+     * get one of controls in product LinearLayout
+     * @param tag type of control to get
+     * @param index number of LineaLayout on the screen
+     * @return control instance
+     */
+    public View getViewByTag(String tag, int index) {
+        LinearLayout temp = (LinearLayout) parentLayout.getChildAt(index);
+        return temp.findViewWithTag(tag);
+    }
+
+    public void computeTotalWeight() {
+        float weight = 0;
+        for (int i = 0; i < productsCount; i++) {
+            weight += Float.parseFloat(((EditText) getViewByTag("txtAmount", i)).getText().toString());
+        }
+        EditText totalWeight = (EditText) findViewById(R.id.editTextWeight);
+        totalWeight.setText(Float.toString(weight));
+    }
+
+    /**
+     * searches for the product in the list using id
+     * @param productsList list of products requested from DB
+     * @param num Spinner selected item index
+     * @return Spinner item details for calculation of totals
+     */
+    public Product getSelectedProduct(ArrayList<Product> productsList, int num) {
+        for (int i = 0; i < productsList.size(); i++) {
+            if (productsList.get(i).getProduct_ID() == ids.get(num)) {
+                return productsList.get(i);
+            }
+        }
+        return new Product(); //should never happen
+    }
+
     public void onClickCalculate() {
-        LinearLayout parentLayout = (LinearLayout) findViewById(R.id.parentLayout);
-        int productsCount = parentLayout.getChildCount() - 1;
         String[] productsIds = new String[productsCount];
         for (int i = 0; i < productsCount; i++) {
-            productsIds[i]=getProductId(parentLayout,i);
+            productsIds[i] = getProductId(i);
         }
-        ProductDB productDB=new ProductDB(getApplicationContext());
-        ArrayList<Product>productsList= productDB.getProductsById(productsIds);
-        float totalProteins=0,totalFats=0,totalCarbohydrates=0,totalCalories=0;
+        ProductDB productDB = new ProductDB(getApplicationContext());
+        ArrayList<Product> productsList = productDB.getProductsById(productsIds);
+        float totalProteins = 0, totalFats = 0, totalCarbohydrates = 0, totalCalories = 0;
         for (int i = 0; i < productsCount; i++) {
-//            Product temp=
+            float amount = Float.parseFloat(((EditText) getViewByTag("txtAmount", i)).getText().toString());
+            Product tempProduct = getSelectedProduct(productsList, (int) (((Spinner) getViewByTag("spnProduct", i)).getSelectedItemId()));
+            totalProteins += tempProduct.proteins * amount;
+            totalFats += tempProduct.fats * amount;
+            totalCarbohydrates += tempProduct.carbohydrates * amount;
+            totalCalories += tempProduct.calories * amount;
         }
+
+        ((TextView) findViewById(R.id.textViewCalories)).setText("Calories:         " + totalCalories);
+        ((TextView) findViewById(R.id.textViewProteins)).setText("Proteins:         " + totalProteins);
+        ((TextView) findViewById(R.id.textViewFats)).setText("Fats:             " + totalFats);
+        ((TextView) findViewById(R.id.textViewCarbohydrates)).setText("Carbohydrates:    " + totalCarbohydrates);
     }
 
     public void onClickRemove(View view) {
-        LinearLayout parentLayout = (LinearLayout) findViewById(R.id.parentLayout);
         parentLayout.removeView(view);
+        productsCount = parentLayout.getChildCount() - 2;
+        computeTotalWeight();
     }
 
     public void onClickAdd() {
-        LinearLayout parentLayout = (LinearLayout) findViewById(R.id.parentLayout);
         LayoutInflater layoutInflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         final LinearLayout itemLayout = (LinearLayout) layoutInflater.inflate(R.layout.product_layout, parentLayout, false);
         Spinner spinner = (Spinner) itemLayout.findViewWithTag("spnProduct");
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, labels);
 
+        final EditText editText = (EditText) itemLayout.findViewWithTag("txtAmount");
+        editText.setText("0");
+
+        editText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    computeTotalWeight();
+                }
+            }
+        });
         // Drop down layout style - list view with radio button
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
@@ -70,6 +133,7 @@ public String getProductId(LinearLayout parentLayout, int i){
             }
         });
         parentLayout.addView(itemLayout, parentLayout.getChildCount() - 2);
+        productsCount = parentLayout.getChildCount() - 2;
     }
 
     @Override
@@ -78,11 +142,16 @@ public String getProductId(LinearLayout parentLayout, int i){
         setContentView(R.layout.activity_main);
 
         ProductDB productDB = new ProductDB(getApplicationContext());
+        parentLayout = (LinearLayout) findViewById(R.id.parentLayout);
+        productsCount = parentLayout.getChildCount() - 2;
         productsList = productDB.getProductList();
         labels = new ArrayList<String>();
+        ids = new ArrayList<Integer>();
         labels.add("select");
+        ids.add(-1);
         for (HashMap<String, String> item : productsList) {
             labels.add(item.get("name"));
+            ids.add(Integer.parseInt(item.get("id")));
         }
         onClickAdd();
 
--- app/src/main/java/com/example/culinarycalculator/ProductDB.java
+++ app/src/main/java/com/example/culinarycalculator/ProductDB.java
@@ -3,10 +3,12 @@ package com.example.culinarycalculator;
 /**
  * Created by str on 023 23/08.
  */
+
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -23,7 +25,7 @@ public class ProductDB {
         SQLiteDatabase db = dbHelper.getWritableDatabase();
         ContentValues values = new ContentValues();
         values.put(Product._NAME, product.name);
-        values.put(Product._CALORIES,product.calories);
+        values.put(Product._CALORIES, product.calories);
         values.put(Product._PROTEINS, product.proteins);
         values.put(Product._FATS, product.fats);
         values.put(Product._CARBOHYDRATES, product.carbohydrates);
@@ -38,7 +40,7 @@ public class ProductDB {
 
         SQLiteDatabase db = dbHelper.getWritableDatabase();
         // It's a good practice to use parameter ?, instead of concatenate string
-        db.delete(Product.TABLE, Product._ID + "= ?", new String[] { String.valueOf(product_ID) });
+        db.delete(Product.TABLE, Product._ID + "= ?", new String[]{String.valueOf(product_ID)});
         db.close(); // Closing database connection
     }
 
@@ -48,20 +50,20 @@ public class ProductDB {
         ContentValues values = new ContentValues();
 
         values.put(Product._NAME, product.name);
-        values.put(Product._CALORIES,product.calories);
+        values.put(Product._CALORIES, product.calories);
         values.put(Product._PROTEINS, product.proteins);
         values.put(Product._FATS, product.fats);
         values.put(Product._CARBOHYDRATES, product.carbohydrates);
 
         // It's a good practice to use parameter ?, instead of concatenate string
-        db.update(Product.TABLE, values, Product._ID + "= ?", new String[] { String.valueOf(product.product_ID) });
+        db.update(Product.TABLE, values, Product._ID + "= ?", new String[]{String.valueOf(product.product_ID)});
         db.close(); // Closing database connection
     }
 
     public ArrayList<HashMap<String, String>> getProductList() {
         //Open connection to read only
         SQLiteDatabase db = dbHelper.getReadableDatabase();
-        String selectQuery =  "SELECT  " +
+        String selectQuery = "SELECT  " +
                 Product._ID + "," +
                 Product._NAME +
                 " FROM " + Product.TABLE;
@@ -88,32 +90,42 @@ public class ProductDB {
 
     }
 
-    public ArrayList<Product> getProductsById(String[]ids){
+    public String constructWhereClause(String[]ids){
+        String temp="("+ids[0];
+        for(int i=1;i<ids.length;i++){
+            temp+=", "+ids[i];
+        }
+        temp+=")";
+        return temp;
+    }
+
+    public ArrayList<Product> getProductsById(String[] ids) {
         SQLiteDatabase db = dbHelper.getReadableDatabase();
-        String selectQuery =  "SELECT  " +
-                Product._ID + "," +
-                Product._NAME + "," +
-                Product._CALORIES + "," +
-                Product._PROTEINS +
-                Product._FATS +
+        String selectQuery = "SELECT  " +
+                Product._ID + ", " +
+                Product._NAME + ", " +
+                Product._CALORIES + ", " +
+                Product._PROTEINS +", " +
+                Product._FATS +", " +
                 Product._CARBOHYDRATES +
                 " FROM " + Product.TABLE
                 + " WHERE " +
-                Product._ID + "=?";// It's a good practice to use parameter ?, instead of concatenate string
+                Product._ID + " IN "+
+                constructWhereClause(ids);
 
-ArrayList<Product>productsList=new ArrayList<Product>();
-        Cursor cursor = db.rawQuery(selectQuery, ids );
+        ArrayList<Product> productsList = new ArrayList<Product>();
+        Cursor cursor = db.rawQuery(selectQuery, ids);
 
         if (cursor.moveToFirst()) {
             do {
                 Product product = new Product();
-                product.product_ID =cursor.getInt(cursor.getColumnIndex(Product._ID));
-                product.name =cursor.getString(cursor.getColumnIndex(Product._NAME));
-                product.calories  =cursor.getInt(cursor.getColumnIndex(Product._CALORIES));
-                product.proteins =cursor.getInt(cursor.getColumnIndex(Product._PROTEINS));
-                product.fats =cursor.getInt(cursor.getColumnIndex(Product._FATS));
-                product.carbohydrates=cursor.getInt(cursor.getColumnIndex(Product._CARBOHYDRATES));
-productsList.add(product);
+                product.product_ID = cursor.getInt(cursor.getColumnIndex(Product._ID));
+                product.name = cursor.getString(cursor.getColumnIndex(Product._NAME));
+                product.calories = cursor.getInt(cursor.getColumnIndex(Product._CALORIES));
+                product.proteins = cursor.getInt(cursor.getColumnIndex(Product._PROTEINS));
+                product.fats = cursor.getInt(cursor.getColumnIndex(Product._FATS));
+                product.carbohydrates = cursor.getInt(cursor.getColumnIndex(Product._CARBOHYDRATES));
+                productsList.add(product);
             } while (cursor.moveToNext());
         }
 
